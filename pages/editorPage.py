aba6d0ccd9b98b60dc39a026cb5f855a51601c0d
jihuni0507 <jihuni0507@gmail.com>
2025-08-19 08:59:09 +0900

    이미지 스케일링 및 이미지 에디터 기능 추가

--- pages/editorPage.py
+++ pages/editorPage.py
@@ -1,8 +1,8 @@
 import os
 
 from PyQt5.QtWidgets import QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QListWidget, QTextEdit
-from PyQt5.QtWidgets import QAction, QFileDialog, QTextEdit, QListWidgetItem, QSizePolicy, QDialog
-from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QAction, QFileDialog, QTextEdit, QListWidgetItem, QSizePolicy, QDialog, QSlider
+from PyQt5.QtGui import QIcon, QPixmap, QPainter, QColor
 from PyQt5.QtCore import Qt, QSize
 
 from .imageEditorPopup import ImageEditorPopup
@@ -15,6 +15,14 @@ class EditorPage(QWidget):
     self.initUI()
 
   def initUI(self):
+    ### 필요한 데이터 / 변수 ###
+    self.pageSize = None
+    self.pageRatio = 1
+    self.pageMargin = 0
+
+    self.original_pixmap = None # 현재 미리보기에서 보이는 이미지의 원본
+
+    #########################
     ### 레이아웃 ###
     ## 좌상단 레이아웃 - 썸네일 + 목록 ##
     self.layout_left = QVBoxLayout()
@@ -29,10 +37,30 @@ class EditorPage(QWidget):
     self.preview_label.setAlignment(Qt.AlignCenter)
     self.preview_label.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored) # QLabel이 조금씩 커지는 현상 방지
     self.layout_right.addWidget(QLabel("이미지 편집 화면"), stretch=1)
+    self.btn_adjustImg = QPushButton("이미지를 페이지 크기에 맞춤")
+    self.layout_right.addWidget(self.btn_adjustImg, stretch=1)
+    # 이미지 크기 조절 슬라이더 #
+    self.slider = QSlider(Qt.Horizontal)
+    self.slider.setMinimum(10)
+    self.slider.setMaximum(200)
+    self.slider.setValue(100)
+    self.slider.setTickPosition(QSlider.TicksBelow)
+    self.slider.setTickInterval(10)
+    self.layout_right.addWidget(self.slider, stretch = 1)
+    
     self.layout_right.addWidget(self.preview_label, stretch=15)
     self.preview_label.setStyleSheet("border-style: solid;"
                                      "border-width: 2px;"
-                                     "border-color: #FFFFFF")
+                                     "border-color: #FFFFFF;"
+                                     "background-color: lightblue;")
+    
+    self.previewImg_label = QLabel("실제 이미지", self.preview_label)
+    self.previewImg_label.setStyleSheet("background-color: transparent;"
+                                        "border-style: solid;"
+                                        "border-width: 2px;"
+                                        "border-color: red;")
+    self.previewImg_label.setAlignment(Qt.AlignCenter)
+    
     
     ## 하단 레이아웃 - 버튼 ##
     self.layout_bottom = QHBoxLayout()
@@ -59,6 +87,9 @@ class EditorPage(QWidget):
 
     ### 이벤트 처리 ###
     self.preview_index = 0
+
+    self.btn_adjustImg.clicked.connect(self.adjustImgtoPage)
+    self.slider.valueChanged.connect(self.scalePreviewImg)
 
     self.btn_openImg.clicked.connect(self.showFileDialog)
     self.btn_editImg.clicked.connect(self.openImageEditor)
@@ -91,6 +122,28 @@ class EditorPage(QWidget):
       if self.image_list.count() == 1:
         self.displayImage(file_path)
 
+  ### 미리보기 이미지를 페이지 크기에 채움
+  def adjustImgtoPage(self):
+    pixmap = self.preview_label.pixmap()
+    if pixmap:
+      width = pixmap.width()
+      height = pixmap.height()
+      if height / width > self.pageRatio: #가로여백이 남을때
+        new_scale = height / self.previewImg_label.height()
+      else: # 세로 여백이 남을때
+        new_scale = width / self.previewImg_label.width()
+      new_size = QSize(int(width / new_scale), int(height / new_scale))
+      scaled_pixmap = pixmap.scaled(new_size, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+      self.preview_label.setPixmap(scaled_pixmap)
+
+  def scalePreviewImg(self, value):
+    factor = value / 100
+    pixmap = self.original_pixmap
+    if pixmap:
+      new_size = pixmap.size() * factor
+      scaled_pixmap = pixmap.scaled(new_size, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+      self.preview_label.setPixmap(scaled_pixmap)
+
   def openImageEditor(self):
     currentItem = self.image_list.currentItem()
     if currentItem:
@@ -99,7 +152,9 @@ class EditorPage(QWidget):
       if editor.exec_() == QDialog.Accepted:
         cropped_pixmap = editor.cropped_pixmap
         self.preview_label.setPixmap(cropped_pixmap)
+        self.original_pixmap = cropped_pixmap
 
+  ### 미리보기 저장 ###
   def savePreview(self):
     index = self.preview_index
     file_name = os.path.basename(self.image_paths[index])
@@ -110,11 +165,35 @@ class EditorPage(QWidget):
     save_folder = QFileDialog.getExistingDirectory(self, "저장할 폴더 선택","./")
     if save_folder:
       save_path = os.path.join(save_folder, f"{name}_cropped.png")
+
+      ##### 설정한 페이지 크기보다 이미지가 작은 경우 사용자가 설정한 단색으로 여백 채우기 #####
+      img_width = pixmap.width()
+      img_height = pixmap.height()
+      new_width = self.previewImg_label.width()
+      new_height = self.previewImg_label.height()
+
+      if img_width < new_width or img_height < new_height:
+        margin_width = (new_width - img_width) // 2
+        margin_height = (new_height - img_height) // 2
+
+        new_pixmap = QPixmap(new_width, new_height)
+        new_pixmap.fill(QColor("black"))
+
+        painter = QPainter(new_pixmap)
+        painter.drawPixmap(margin_width, margin_height, pixmap)
+        painter.end()
+        pixmap = new_pixmap
+
       pixmap.save(save_path, "PNG")
   
+  ### 페이지 설정 관련 ###
   def openPageSetting(self):
     pageSetting = PageSettingPopup(self)
+    pageSetting.sig_pageSettingConfirmed.connect(self.applyPageSettings) # 팝업창에서 정보를 받음
     pageSetting.exec_()
+  def applyPageSettings(self, settings):
+    self.pageSize = settings["pageSize"]
+    self.updatePreviewGeometry()
 
   def createPDFfile(self):
     convert2PDF(self.img_sources, [], pdf_path='C:\\GIST\\dev\\pdfConverter\\ouput.pdf') ### 임시, 꼭 크롭한 이미지 파일이 들어가도록 수정!!! 
@@ -129,5 +208,24 @@ class EditorPage(QWidget):
     index = self.image_list.row(item)
     self.preview_index = index
     pixmap = QPixmap(self.image_paths[index])
-    scaled_pixmap = pixmap.scaled(self.preview_label.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
-    self.preview_label.setPixmap(scaled_pixmap)
+    self.original_pixmap = pixmap
+    # scaled_pixmap = pixmap.scaled(self.preview_label.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+    self.preview_label.setPixmap(self.original_pixmap)
+
+  ### 창의 크기가 변경될 때 ###
+  def resizeEvent(self, event):
+    super().resizeEvent(event)
+    self.updatePreviewGeometry()
+
+  def updatePreviewGeometry(self):
+    preview_width = self.preview_label.width()
+    preview_height = self.preview_label.height()
+
+    if self.pageSize == "A4":
+      self.pageRatio = 1.414
+    child_height = preview_height
+    child_width = int(child_height / self.pageRatio) ### 페이지 크기 설정에 따라 유동적으로 바뀌어야 함
+    x = (preview_width - child_width) // 2
+    y = 0
+
+    self.previewImg_label.setGeometry(x, y, child_width, child_height)
--- pages/pageSettingPopup.py
+++ pages/pageSettingPopup.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QDialog, QComboBox, QLabel, QLineEdit, QPushButton, QFileDialog
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, pyqtSignal
 
 class PageSettingPopup(QDialog):
   def __init__(self, parent=None):
@@ -22,6 +22,8 @@ class PageSettingPopup(QDialog):
     cb_pageSize.addItem('커스텀')
     cb_pageSize.activated[str].connect(self.setPageSize)
 
+    self.PageSize = QLineEdit()
+
     ## 여백 / 재단 설정 ##
     self.label_padding = QLabel('재단 길이 설정', self)
     textEdit_padding = QLineEdit()
@@ -30,6 +32,7 @@ class PageSettingPopup(QDialog):
     self.layout_pdfSetting = QHBoxLayout()
     self.pdf_path = QLineEdit()
     btn_pdf_path = QPushButton('PDF 저장 경로 선택')
+    self.layout_pdfSetting.addWidget(self.PageSize) ## 임시
     self.layout_pdfSetting.addWidget(self.pdf_path)
     self.layout_pdfSetting.addWidget(btn_pdf_path)
 
@@ -48,7 +51,7 @@ class PageSettingPopup(QDialog):
     self.btn_cancel.clicked.connect(self.reject)
     self.btn_cancel.setAutoDefault(False)
     self.btn_ok = QPushButton('확인')
-    self.btn_ok.clicked.connect(self.accept)
+    self.btn_ok.clicked.connect(self.sendSettingAndClose)
     self.btn_ok.setDefault(True)
     self.btn_ok.setAutoDefault(True)
     layout_btn.addWidget(self.btn_cancel, stretch=2)
@@ -66,10 +69,22 @@ class PageSettingPopup(QDialog):
     ## 이벤트 핸들러 ##
     btn_pdf_path.clicked.connect(self.showPathDialog)
 
+  ## 페이지 선택 이후 ##
   def setPageSize(self, text):
-    return 0
+    self.PageSize.setText(text)
   
   def showPathDialog(self):
     pdf_folder = QFileDialog.getExistingDirectory(self, 'PDF를 저장할 폴더 선택', "./")
     if pdf_folder:
       self.pdf_path.setText(pdf_folder)
+
+  sig_pageSettingConfirmed = pyqtSignal(dict) # 딕셔너리 타입을 인자로 넘김
+  
+  def sendSettingAndClose(self):
+    settings = {
+      "pageSize": self.PageSize.text(),
+      "pdfPath": self.pdf_path.text()
+    }
+    self.sig_pageSettingConfirmed.emit(settings)
+    self.accept()
+
